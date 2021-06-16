c85a8151427ebff5f15616015a7f3dbd033b96a3
jecsham <jecsham27@gmail.com>
2021-06-16 10:21:29 -0500

    add seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="title" content="Sensor Stream | Jecsham Castillo" />
+          <meta
+            name="description"
+            content="Sensor Stream allows you to stream sensor data of pc components like gpu, cpu, ram, storage, etc, of your PC through UDP datagrams or WebSockets."
+          />
+          <meta name="keywords" content="sensor stream, hardware monitor" />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             type="text/css"
             href="https://cdn.jsdelivr.net/gh/pgrabovets/json-view@master/dist/jsonview.bundle.css"
           />
+
           <script src="https://cdn.jsdelivr.net/gh/pgrabovets/json-view@master/dist/jsonview.bundle.js"></script>
         </Head>
         <body>
